c6567a9376955abeb4403d9198696007b4a43ecb
EC2 Default User <ec2-user@ip-172-31-13-114.ap-northeast-1.compute.internal>
2022-10-23 14:06:06 +0000

    [Update] 部分テンプレート実装中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,8 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,28 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @book = @user.books
+    @books = @user.books
+    @book = Book.new
   end
 
   def edit
-    @user = User.find(params[:id]) 
+    @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
-  
+
+  def index
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+  end
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
