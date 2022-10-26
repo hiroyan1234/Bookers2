4d0daf505aba212e9d99f65bdc260195a67a2fa6
EC2 Default User <ec2-user@ip-172-31-13-114.ap-northeast-1.compute.internal>
2022-10-26 10:10:50 +0000

    [Update] フラッシュメッセージ追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def after_sign_in_path_for(resource)
-    books_path
+    user_path(current_user)
   end
-  
+
   def after_sign_out_path_for(resource)
-    about_path
+    root_path
   end
-  
+
   protected
-  
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+    added_attrs = [:user_name, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
