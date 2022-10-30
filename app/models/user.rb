d1863001ab4a3e4818a7200324b3f6438f7b67ad
EC2 Default User <ec2-user@ip-172-31-13-114.ap-northeast-1.compute.internal>
2022-10-30 05:25:57 +0000

    [Finish] Bookers2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
 
   has_one_attached :profile_image
-  
-  validates :name, length: {minimum: 2, maximum: 20, message: "is too short (minimum is 2 characters)"}
+
+  validates :name, length: {minimum: 2, maximum: 20, message: "is too short (minimum is 2 characters)"}, uniqueness: true
   validates :introduction, length: {maximum: 50}
 
   def get_profile_image(width, height)
